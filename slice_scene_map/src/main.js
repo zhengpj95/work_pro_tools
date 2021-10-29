f5e0a320d3827be52148a992ec6b74992426e464
zhengpj <598359997@qq.com>
2021-10-29 22:49:25 +0800

    update

--- slice_scene_map/src/main.js
+++ slice_scene_map/src/main.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const images = require('images');
 
-let mapId = 9002; // todo
+let mapId = 10020; // todo
 let eachWidth = 256;
-let eachHeigh = 256;
-let cellHeigh = 32;
+let eachHeight = 256;
 let cellWidth = 32;
+let cellHeight = 32;
 
 let imgData = images(path.join(__dirname, '..', 'resources', `${mapId}.jpg`));
 let outputRoot = path.join(__dirname, '..', 'output', `${mapId}`);
@@ -20,25 +20,25 @@ obj.path = path.normalize(path.join('resources', `${mapId}`));
 obj.width = imgData.width();
 obj.height = imgData.height();
 obj.sliceWidth = eachWidth;
-obj.sliceHeigh = eachHeigh;
-obj.cellHeigh = cellHeigh;
+obj.sliceHeight = eachHeight;
 obj.cellWidth = cellWidth;
+obj.cellHeight = cellHeight;
 obj.blocks = [];
 
-for (let i = 0; i < Math.floor(obj.height / obj.cellHeigh); i++) {
+for (let i = 0; i < Math.floor(obj.height / obj.cellHeight); i++) {
 	let cols = Math.floor(obj.width / obj.cellWidth);
 	let row = new Array(cols).fill(0);
 	obj.blocks.push(row);
 }
 
 // 地图切片
-let cols = Math.floor(imgData.width() / eachHeigh);
-let rows = Math.floor(imgData.height() / eachHeigh);
+let cols = Math.floor(imgData.width() / eachHeight);
+let rows = Math.floor(imgData.height() / eachHeight);
 for (let i = 0; i < rows; i++) {
 	for (let j = 0; j < cols; j++) {
 		let x = j * eachWidth;
-		let y = i * eachHeigh;
-		let img = images(eachWidth, eachHeigh).copyFromImage(imgData, x, y, eachWidth, eachHeigh);
+		let y = i * eachHeight;
+		let img = images(eachWidth, eachHeight).copyFromImage(imgData, x, y, eachWidth, eachHeight);
 		img.saveAsync(path.join(outputRoot, `${i}_${j}.jpg`), 'jpg', (err) => {
 			if (err) {
 				console.log(`创建 ${i}_${j}.jpg 失败`);
@@ -48,7 +48,7 @@ for (let i = 0; i < rows; i++) {
 }
 
 // 小地图
-imgData.resize(eachWidth, eachHeigh);
+imgData.resize(eachWidth, eachHeight);
 imgData.saveAsync(path.join(outputRoot, 'mini.jpg'), 'jpg', (err) => {
 	if (err) {
 		console.log(`创建 mini.jpg 失败`);
