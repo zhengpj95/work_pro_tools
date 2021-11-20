d9c9bda061fb94a2c68828d5f7a3064bacff2370
zhengpj <598359997@qq.com>
2021-11-20 10:52:23 +0800

    update

--- excel2json/str2list.py
+++ excel2json/str2list.py
@@ -1,7 +1,10 @@
-""" 
+"""
 把字符串数组解析成数组
 如果是number类型的，应该做对应转换
 """
+
+from enum import Flag
+import re
 
 
 def strToList(lab: str):
@@ -42,12 +45,40 @@ def strToList(lab: str):
     if len(stack) == 1:
         stack = stack.pop()
     # print(stack, len(stack))
-    return stack
+    return dealNumberList(stack)
+
+
+def dealNumberList(array):
+    result = []
+    for i in array:
+        if type(i).__name__ == 'list':
+            res = dealNumberList(i)
+            result.append(res)
+        else:
+            if is_number(i):
+                if i.find('.') > -1:
+                    i = float(i)
+                else:
+                    i = int(i)
+            if i == '':
+                result.append(None)
+            else:
+                result.append(i)
+    return result
+
+
+def is_number(s):
+    if not s:
+        return False
+    value = re.compile(r'^[-+]?[.]?[0-9]+')
+    if value.match(s):
+        return True
+    return False
 
 
 """ test """
 
-# label = '[1,2,[11,31],33,44,[4,5,6,,  7]]'
+# label = '[1,2,[11,31],33,44,[4,5,6,,  7],123123,43229001111100,10000.10010,"你好中国",["北京","中国","china"]]'
 # print(len(label), label.find('['), label.rfind(']'))
 # print(label[label.find('[')+1: label.rfind(']')].split(','))
 # res = strToList(label)
@@ -56,3 +87,10 @@ def strToList(lab: str):
 # print(label.find(']'))
 # label = '[1,2,[11,31],33,44,[4,5,6,,  7]]'
 # print(re.split(r'[\,\[\]]', label))
+
+# print(is_number('123.123'))
+# print(is_number('123.123asd'))
+# print(is_number('123'))
+# print(is_number('-123.00'))
+# print(is_number('+123.0'))
+# print(is_number('六'))
