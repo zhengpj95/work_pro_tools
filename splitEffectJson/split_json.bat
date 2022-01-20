e539620ef05e736718be38ae3495c15082d293b7
zhengpj <598359997@qq.com>
2022-01-20 16:56:49 +0800

    修改路径

--- splitEffectJson/split_json.bat
+++ splitEffectJson/split_json.bat
@@ -2,11 +2,11 @@
 
 set drawIn=%~1
 @REM 此文件所在的根目录
-set srcRoot=E:\\git-project\\work_pro_tools\\splitEffectJson
+set srcRoot=E:\\git_project\\work_pro_tools\\splitEffectJson
 @REM 要拆图的目录
-set source=E:\\git-project\\work_pro_tools\\splitEffectJson\\effect_source
+set source=E:\\git_project\\work_pro_tools\\splitEffectJson\\effect_source
 @REM 拆图输出目录
-set output=E:\\git-project\\work_pro_tools\\splitEffectJson\\output
+set output=E:\\git_project\\work_pro_tools\\splitEffectJson\\output
 
 node %srcRoot%\src\splitAnim.js %drawIn% %source% %output%
 pause
