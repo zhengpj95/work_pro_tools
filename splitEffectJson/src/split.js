ed5a8226f9bb18bc3de7997c111f155fc916185b
zhengpj <598359997@qq.com>
2022-03-31 10:17:04 +0800

    update

--- splitEffectJson/src/split.js
+++ splitEffectJson/src/split.js
@@ -2,18 +2,18 @@
  * 把图集分解
  * 注意：是根据 labels 子项，得到对应的 frames 的子项，再从 res 中切割图片出来
  */
-const path = require('path');
-const fs = require('fs');
-const images = require('images');
+const path = require("path");
+const fs = require("fs");
+const images = require("images");
 
-let mcName = 'scenePlayer_0';
+let mcName = "scenePlayer_1";
 
-let jsonData = fs.readFileSync(path.join(__dirname, `../effect_source/${mcName}.json`), 'utf-8');
+let jsonData = fs.readFileSync(path.join(__dirname, `../effect_source/${mcName}.json`), "utf-8");
 jsonData = JSON.parse(jsonData);
 let pngSource = images(path.join(__dirname, `../effect_source/${mcName}.png`));
 
 function dealLabels() {
-	let labels = jsonData['mc'][mcName]['labels'];
+	let labels = jsonData["mc"][mcName]["labels"];
 	if (!labels || !labels.length) {
 		console.log(`没有 labels 字段`);
 		return;
@@ -28,15 +28,15 @@ function dealFrames(lb) {
 	if (!lb) {
 		return;
 	}
-	let frames = jsonData['mc'][mcName]['frames'];
+	let frames = jsonData["mc"][mcName]["frames"];
 	if (!frames || !frames.length) {
 		console.log(`没有 frames 字段`);
 		return;
 	}
-	let startFrame = lb['frame'];
-	let endFrame = lb['end'];
+	let startFrame = lb["frame"];
+	let endFrame = lb["end"];
 	for (let i = startFrame, j = 0; i <= endFrame; i++) {
-		dealRes(lb['name'], j++, frames[i - 1]);
+		dealRes(lb["name"], j++, frames[i - 1]);
 	}
 }
 
@@ -44,7 +44,7 @@ function dealRes(name, idx, frame) {
 	if (!frame) {
 		return;
 	}
-	let output = path.join(__dirname, '../output/', mcName);
+	let output = path.join(__dirname, "../output/", mcName);
 	if (!fs.existsSync(output)) {
 		fs.mkdirSync(output);
 	}
@@ -53,7 +53,7 @@ function dealRes(name, idx, frame) {
 		fs.mkdirSync(output);
 	}
 
-	let res = jsonData['res'][`${frame['res']}`];
+	let res = jsonData["res"][`${frame["res"]}`];
 	if (!res) {
 		console.log(`没有 res 字段`);
 		return;
