7f0e262fabd001a88e181a1e051894af746ebc2c
zhengpj <598359997@qq.com>
2022-11-06 11:46:31 +0800

    update clip.js

--- splitEffectJson/src/clip.js
+++ splitEffectJson/src/clip.js
@@ -8,7 +8,7 @@
  *     ......
  *   }
  * }
- * 
+ *
  * 从一张合图中裁剪出每一帧图片，每一帧图片就是w*h
  */
 
@@ -17,17 +17,18 @@ const fs = require("fs");
 const images = require("images");
 
 //需要剪裁的图片 todo
-let source = '../effect_source/stand_3';
+let source = "../effect_source/stand_3";
+let output = path.join(__dirname, "../output");
 
 /**
  * 读取json里面的图片资源信息数组
  */
 function readJson() {
 	let infoMap = {};
-	let jsonStr = fs.readFileSync(source + '.json', 'utf-8');
-	let jsonObj = JSON.parse(jsonStr)['frames']
+	let jsonStr = fs.readFileSync(source + ".json", "utf-8");
+	let jsonObj = JSON.parse(jsonStr)["frames"];
 	for (let key in jsonObj) {
-		infoMap[key] = jsonObj[key]
+		infoMap[key] = jsonObj[key];
 	}
 	return infoMap;
 }
@@ -36,23 +37,34 @@ function readJson() {
  * 处理一帧的图片资源，并保存
  */
 function dealSingleImg(pngSource, imgInfo, name) {
-	let dirname = path.basename(source);
-	let output = path.join(__dirname, '../output/', dirname);
 	if (!fs.existsSync(output)) {
-		fs.mkdirSync(output)
+		fs.mkdirSync(output);
 	}
 
 	images(imgInfo.w, imgInfo.h)
 		.copyFromImage(pngSource, imgInfo.x, imgInfo.y, imgInfo.w, imgInfo.h)
-		.save(path.join(output, `${name}.png`))
+		.save(path.join(output, `${name}.png`));
+	console.log(`......${name}.png`);
 }
 
 function main() {
+	console.log(`============= 剪裁图集开启 =============`);
 	let infoMap = readJson();
-	let pngSource = images(source + '.png');
+	let pngSource = images(source + ".png");
 	for (let frame in infoMap) {
-		dealSingleImg(pngSource, infoMap[frame], frame)
+		dealSingleImg(pngSource, infoMap[frame], frame);
 	}
+	console.log(`============= 剪裁图集完成 =============`);
 }
 
-main();
+if (process.argv.length > 2) {
+	let argv = process.argv.splice(2);
+	source = argv[0].replace(path.extname(argv[0]), "");
+	output = path.join(output, path.basename(source));
+	// console.log(argv, source, output);
+	main();
+} else {
+	source = "../effect_source/stand_3";
+	output = path.join(output, "stand_3");
+	main();
+}
